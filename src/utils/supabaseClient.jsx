b2f272907e1039e994a07332db9b3ebfcafdd76b
kounoti <ac09606gc8xkh@gmail.com>
2024-03-13 01:15:26 +0900

    supabaseClient.jsxのpersistSessionをfalseからtrueに変更した

--- src/utils/supabaseClient.jsx
+++ src/utils/supabaseClient.jsx
@@ -5,6 +5,6 @@ const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
-    persistSession: false,
+    persistSession: true,
   },
 });
