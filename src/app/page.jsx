539f599093edae82d7bb2686cbabf34b83e57ed7
kounoti <ac09606gc8xkh@gmail.com>
2024-03-14 00:31:40 +0900

    トレーニング開始ボタンがスマホサイズで違和感なく表示されるように修正

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ export default function Home() {
       <div className="flex justify-center items-center m-10">
         <Link
           href="/components/Training"
-          className="bg-blue-300 hover:bg-blue-500 text-white font-bold text-5xl py-10 px-10 rounded border-2 border-blue-400"
+          className="bg-blue-300 hover:bg-blue-500 text-white font-bold py-10 px-10 rounded border-2 border-blue-400 sm:text-5xl text-2xl "
         >
           トレーニング開始
         </Link>
