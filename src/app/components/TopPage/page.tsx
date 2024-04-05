6b9675ee283417f808d246fe2a4445d834d0a4ae
kounoti <ac09606gc8xkh@gmail.com>
2024-04-06 01:58:19 +0900

    Topに遷移するときにAuthenticaionページを通らないようにコード修正

--- src/app/components/TopPage/page.tsx
+++ src/app/components/TopPage/page.tsx
@@ -1,17 +1,9 @@
-import Image from "next/image";
-import Link from "next/link";
+import TopPage from "./TopPage";
 
 export default function Page() {
   return (
     <>
-      <div className="flex justify-center items-center m-10">
-        <Link
-          href="/components/Training"
-          className="bg-blue-300 hover:bg-blue-500 text-white font-bold py-10 px-10 rounded border-2 border-blue-400 sm:text-5xl text-2xl "
-        >
-          トレーニング開始
-        </Link>
-      </div>
+      <TopPage />
     </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react";
 import Authentication from "./components/Authentication/page";
 import "./globals.css";
+import { supabase_google } from "./components/Authentication/SupabaseGoogle";
+import TopPage from "./components/TopPage/TopPage";
 
 export default function Home() {
+  const [currentUser, setCurrentUser] = useState<string | null>(null);
+
+  // 現在ログインしているユーザーを取得する処理
+  const getCurrentUser = async () => {
+    // ログインのセッションを取得する処理
+    const { data } = await supabase_google.auth.getSession();
+    // セッションがあるときだけ現在ログインしているユーザーを取得する
+    if (data.session !== null) {
+      // supabaseに用意されている現在ログインしているユーザーを取得する関数
+      const {
+        data: { user },
+      } = await supabase_google.auth.getUser();
+      // currentUserにユーザーのメールアドレスを格納
+      setCurrentUser(user?.email ?? null);
+    }
+  };
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
+
   return (
     <>
-      <Authentication />
+      {currentUser === null ? <Authentication /> : <TopPage />}
       {/* <div className="flex justify-center items-center m-10">
         <Link
           href="/components/Training"
