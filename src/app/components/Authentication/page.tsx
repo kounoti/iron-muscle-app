4a520df3f9cbe679dcd9b54fbb156bd8d91a8083
kounoti <ac09606gc8xkh@gmail.com>
2024-04-05 23:05:51 +0900

    supabase_googleの型を削除

--- src/app/components/Authentication/page.tsx
+++ src/app/components/Authentication/page.tsx
@@ -10,7 +10,7 @@ import {
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
-const supabase_google: SupabaseClient = createClient(
+const supabase_google = createClient(
   "https://vjwvrqtjtwowuxxxznoq.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqd3ZycXRqdHdvd3V4eHh6bm9xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk5NjgzMjksImV4cCI6MjAyNTU0NDMyOX0.bNNhGE_PIb2NqQh9Id-KxjzfClSw_rANLTX7cOz3iHM"
 );
