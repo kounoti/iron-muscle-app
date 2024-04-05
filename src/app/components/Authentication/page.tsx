6eb1eac33b4e3a07c5239e244d9209eb0f736679
kounoti <ac09606gc8xkh@gmail.com>
2024-04-06 01:02:05 +0900

    AuthenticationのメインコンポーネントをPageに変更

--- src/app/components/Authentication/page.tsx
+++ src/app/components/Authentication/page.tsx
@@ -11,7 +11,7 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { supabase_google } from "./SupabaseGoogle";
 
-export default function Authentication() {
+export default function Page() {
   const [currentUser, setCurrentUser] = useState<string>("");
   const router = useRouter();
 
