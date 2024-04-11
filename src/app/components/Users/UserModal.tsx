9c0e068541180d21d2ed1623fb5b0649a8d08072
kounoti <ac09606gc8xkh@gmail.com>
2024-04-12 00:07:13 +0900

    router.push("/components/Users");に修正

--- src/app/components/Users/UserModal.tsx
+++ src/app/components/Users/UserModal.tsx
@@ -44,7 +44,7 @@ const UserModal: React.FC<UserModalProps> = ({
       .eq("user_account", account);
 
     onClose();
-    router.push("/Users");
+    router.push("/components/Users");
     router.refresh();
   };
 
