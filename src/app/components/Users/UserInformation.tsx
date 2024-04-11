1a7ae1b60526e8b11a0b9f11345e2efd2cdc90c7
kounoti <ac09606gc8xkh@gmail.com>
2024-04-12 01:53:18 +0900

    loading画面を調整した

--- src/app/components/Users/UserInformation.tsx
+++ src/app/components/Users/UserInformation.tsx
@@ -42,7 +42,14 @@ const UserInformation: React.FC<userInformationProps> = ({ account }) => {
 
   // userがnullの場合、データの取得がまだ行われていないことを示す
   if (!user) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="text-center">
+          <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-gray-900"></div>
+          <div>Loading...</div>
+        </div>
+      </div>
+    );
   }
 
   return (
