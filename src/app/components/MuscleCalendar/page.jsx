2e1f7115dd68935f4a5a6161c4ef3ba0362e4243
kounoti <ac09606gc8xkh@gmail.com>
2024-04-14 14:05:27 +0900

    名前をpageから変更

--- src/app/components/MuscleCalendar/page.jsx
+++ src/app/components/MuscleCalendar/page.jsx
@@ -45,6 +45,6 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 
-export default function Page() {
+export default function MuscleCalendar() {
   return <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />;
 }
