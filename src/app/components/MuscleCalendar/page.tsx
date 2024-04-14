291f80b4f891dc13e32075f3310cb10a10719b6c
kounoti <ac09606gc8xkh@gmail.com>
2024-04-14 13:58:37 +0900

    Calendarの最小限の機能を作成

--- src/app/components/MuscleCalendar/page.tsx
+++ src/app/components/MuscleCalendar/page.tsx
@@ -41,3 +41,10 @@
 //     </>
 //   );
 // }
+
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+
+export default function Page() {
+  return <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />;
+}
