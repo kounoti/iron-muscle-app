e437442b3207b9c71db62a8262fb824a8bf2b9a1
kounoti <ac09606gc8xkh@gmail.com>
2024-04-21 02:11:34 +0900

    トレーニング記録後にモーダルが開くように修正

--- src/app/components/Training/PageBody.tsx
+++ src/app/components/Training/PageBody.tsx
@@ -151,10 +151,12 @@ const PageBody = () => {
         >
           トレーニングを記録する
         </button>
-        <TimeLineModal
-          isOpen={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-        />
+        {isModalOpen && (
+          <TimeLineModal
+            isOpen={isModalOpen}
+            onClose={() => setIsModalOpen(false)}
+          />
+        )}
       </div>
       {/* <br />
       <Link href="/" className="font-bold text-blue-700 text-15 ml-auto">
