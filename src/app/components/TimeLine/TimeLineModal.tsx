73811f406d51a5b862f1bf167d6c37890a7e5aef
kounoti <ac09606gc8xkh@gmail.com>
2024-04-21 01:50:55 +0900

    mr-4を追加

--- src/app/components/TimeLine/TimeLineModal.tsx
+++ src/app/components/TimeLine/TimeLineModal.tsx
@@ -57,13 +57,13 @@ const TimeLineModal: React.FC<UserModalProps> = ({ isOpen, onClose }) => {
         <div className="bg-gray-200 p-4 flex justify-end">
           <button
             onClick={addToUserInformation}
-            className="px-4 py-2 text-sm font-medium text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="mr-4 px-4 py-2 text-sm font-medium text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
           >
             投稿
           </button>
           <button
             onClick={onClose}
-            className="mr-4 px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100 rounded-md hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100 rounded-md hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500"
           >
             投稿しない
           </button>
