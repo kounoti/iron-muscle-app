c3627d140f710de184e28d37f073dcddce36655c
kounoti <ac09606gc8xkh@gmail.com>
2024-04-21 18:42:31 +0900

    画像の位置を修正

--- src/app/components/TopPage/TopPageIn.tsx
+++ src/app/components/TopPage/TopPageIn.tsx
@@ -5,16 +5,20 @@ export default function TopPageIn() {
   return (
     <>
       <div className="flex flex-col justify-center items-center h-screen">
-        <div className="text-center text-black mb-8">
-          <Image
-            src="/gym.png" // 画像のパスを指定
-            alt="Iron Muscle Logo" // 画像の代替テキスト
-            className="h-24 sm:h-32 md:h-40 lg:h-48 xl:h-56 mb-4" // 画像のサイズをデバイスごとに設定
-          />
-          <h1 className="text-5xl font-bold mb-4">
-            Welcome to Iron muscle App!
-          </h1>
-          <p className="text-lg">Let&apos;s work out together!</p>
+        <div className="text-center text-black mb-8 flex items-center">
+          <div className="mr-4">
+            <Image
+              src="/gym.png" // 画像のパスを指定
+              alt="Iron Muscle Logo" // 画像の代替テキスト
+              className="h-24 sm:h-32 md:h-40 lg:h-48 xl:h-56" // 画像のサイズをデバイスごとに設定
+            />
+          </div>
+          <div>
+            <h1 className="text-5xl font-bold mb-4">
+              Welcome to Iron muscle App!
+            </h1>
+            <p className="text-lg">Let&apos;s work out together!</p>
+          </div>
         </div>
         <Link href="/components/Training">
           <a className="bg-teal-500 hover:bg-teal-600 text-white font-bold py-3 px-6 rounded-lg transition-colors duration-300 ease-in-out">
