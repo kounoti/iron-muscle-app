c441de3cd0258eb3b347eab07265a001fbd5e9ca
kounoti <ac09606gc8xkh@gmail.com>
2024-04-21 18:53:52 +0900

    画像の位置を修正

--- src/app/components/TopPage/TopPageIn.tsx
+++ src/app/components/TopPage/TopPageIn.tsx
@@ -8,12 +8,15 @@ export default function TopPageIn() {
         <div className="text-center text-black mb-8">
           {/* 画像を画面中央に配置 */}
           <div className="relative w-40 sm:w-48 md:w-56 lg:w-64 xl:w-72 h-40 sm:h-48 md:h-56 lg:h-64 xl:h-72 mb-4">
-            <Image
-              src="/gym.png" // 画像のパスを指定
-              alt="Iron Muscle Logo" // 画像の代替テキスト
-              layout="fill" // 画像を親要素にフィットさせる
-              objectFit="contain" // 画像を完全に表示する
-            />
+            {/* 修正：Imageコンポーネントの親要素にもrelativeクラスを追加 */}
+            <div className="relative w-full h-full">
+              <Image
+                src="/gym.png" // 画像のパスを指定
+                alt="Iron Muscle Logo" // 画像の代替テキスト
+                layout="fill" // 画像を親要素にフィットさせる
+                objectFit="contain" // 画像を完全に表示する
+              />
+            </div>
           </div>
           <h1 className="text-5xl font-bold mb-4">
             Welcome to Iron muscle App!
@@ -26,16 +29,6 @@ export default function TopPageIn() {
           </a>
         </Link>
       </div>
-
-      {/* <div className="flex justify-center items-center m-10">
-        <Link
-          href="/components/Training"
-          className="bg-blue-300 hover:bg-blue-500 text-white font-bold py-10 px-10 rounded border-2 border-blue-400 sm:text-5xl text-2xl "
-        >
-          トレーニング開始
-        </Link>
-        
-      </div> */}
     </>
   );
 }
