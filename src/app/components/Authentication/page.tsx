c385febbac601d8c4d4635005c98b25150400acf
kounoti <ac09606gc8xkh@gmail.com>
2024-04-26 22:45:47 +0900

    プロフィール画像バグ修正

--- src/app/components/Authentication/page.tsx
+++ src/app/components/Authentication/page.tsx
@@ -35,7 +35,7 @@ export default function Authentication() {
           {
             // 追加するデータを指定
             user_age: 25,
-            user_avatar: `http://api.adorable.io/avatars/200/${user?.email}.png`,
+            user_avatar: "https://picsum.photos/200/300",
             user_height: 170,
             user_weight: 60,
             user_name: "ゲスト",
