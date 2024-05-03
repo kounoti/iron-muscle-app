707d3702c80424a802e0e7f4bd336679e6845892
kounoti <ac09606gc8xkh@gmail.com>
2024-05-03 21:00:51 +0900

    画像の末尾をjpgに修正

--- src/app/components/TopPage/page.tsx
+++ src/app/components/TopPage/page.tsx
@@ -6,7 +6,7 @@ export default function TopPage() {
     <>
       <div className="w-full h-full">
         <Image
-          src="/TopPage_background.png" // 画像のパスを指定
+          src="/TopPage_background.jpg" // 画像のパスを指定
           alt="TopPageTraining" // 画像の代替テキスト
           layout="fill" // 画像を親要素にフィットさせる
           objectFit="contain" // 画像を完全に表示する
