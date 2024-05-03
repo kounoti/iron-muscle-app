c3092bd53ce762f44bc4bcda209d5fedd21a9774
kounoti <ac09606gc8xkh@gmail.com>
2024-05-03 20:57:22 +0900

    トップページに背景画像を追加

--- src/app/components/TopPage/page.tsx
+++ src/app/components/TopPage/page.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
 import TopPageIn from "./TopPageIn";
 
 export default function TopPage() {
   return (
     <>
+      <div className="w-full h-full">
+        <Image
+          src="/TopPage_background.png" // 画像のパスを指定
+          alt="TopPageTraining" // 画像の代替テキスト
+          layout="fill" // 画像を親要素にフィットさせる
+          objectFit="contain" // 画像を完全に表示する
+        />
+      </div>
       {/* {currentUser === null ? <Authentication /> : <TopPageIn />} */}
       <TopPageIn />
     </>
