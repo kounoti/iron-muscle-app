eabd00edd1d69a6191c868dfe95262a4aaa0b1e8
kounoti <ac09606gc8xkh@gmail.com>
2024-05-06 22:58:36 +0900

    モーダルの色を調整

--- src/app/components/Training/TrainingModal/TrainingModal.tsx
+++ src/app/components/Training/TrainingModal/TrainingModal.tsx
@@ -94,19 +94,21 @@ const TrainingModal: React.FC<UserModalProps> = ({
         className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
         aria-hidden="true"
       ></div>
-      <div className="bg-blue-100 rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
+      <div className="bg-blue-500 rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
         <div className="flex justify-end p-2">
           <button onClick={onClose}>
             <IoCloseCircleOutline />
           </button>
         </div>
-        <div className="bg-blue-200 p-4">
-          <h2 className="text-lg font-semibold">トレーニングを続けますか？</h2>
+        <div className="bg-white p-4">
+          <h2 className="text-lg font-semibold text-center">
+            トレーニングを続けますか？
+          </h2>
         </div>
-        <div className="bg-blue-200 p-4 flex justify-end">
+        <div className="bg-white p-4 flex justify-end">
           <button
             onClick={moveMuscleMemory}
-            className="mr-4 px-4 py-2 text-sm font-medium text-gray-700 bg-blue-300 rounded-md hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="mr-4 px-4 py-2 text-sm font-medium text-gray-700 bg-gray-600 rounded-md hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-indigo-500"
           >
             終了
           </button>
