5e525f8f50c17724f23a4b83febcff445bfeb380
kounoti <ac09606gc8xkh@gmail.com>
2024-05-06 19:34:32 +0900

    BOX20%に修正

--- src/app/components/Training/PageBody.tsx
+++ src/app/components/Training/PageBody.tsx
@@ -120,7 +120,7 @@ const PageBody = () => {
     <>
       <div className="max-w-4xl mx-auto">
         <ChakraProvider>
-          <Box mx="10%">
+          <Box mx="20%">
             {/* <div>トレーニングページです</div> */}
             <SelectMuscleParts
               onChildMuscleChange={handleChildMuscleChange}
