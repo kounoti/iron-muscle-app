a2714cf61fcbb92c3eaba084a9eba5ff54e6ac76
kounoti <ac09606gc8xkh@gmail.com>
2024-05-15 22:12:12 +0900

    Memoryのボタンの色を修正

--- src/app/components/Calendar/page.jsx
+++ src/app/components/Calendar/page.jsx
@@ -3,7 +3,34 @@
 import React from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import interactionPlugin from "@fullcalendar/interaction";
 
 export default function Page() {
-  return <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />;
+  function renderEventContent(eventInfo) {
+    return (
+      <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+      </>
+    );
+  }
+
+  const handleDateClick = (arg) => {
+    alert(arg.dateStr);
+    console.log(arg);
+  };
+
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin, interactionPlugin]}
+      initialView="dayGridMonth"
+      // eventContent={renderEventContent}
+      weekends={false}
+      events={[
+        { title: "event 1", date: "2024-05-15" },
+        { title: "event 2", date: "2024-05-20" },
+      ]}
+      dateClick={handleDateClick}
+    />
+  );
 }
