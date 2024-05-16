73ad0fb60afab26df287859fed568462f4b8e7c6
kounoti <ac09606gc8xkh@gmail.com>
2024-05-16 22:27:29 +0900

    日付を1日後に変更できるようにコード修正

--- src/app/components/Calendar/MuscleCalendar.tsx
+++ src/app/components/Calendar/MuscleCalendar.tsx
@@ -61,7 +61,10 @@ const MuscleCalendar: React.FC<MuscleCalendarProps> = ({ account }) => {
 
   // 日付に応じてクラスを追加する関数
   const dayCellClassNames = (arg: any) => {
-    const dateString = arg.date.toLocaleDateString();
+    // 修正：日付を1日後にずらす
+    const date = new Date(arg.date);
+    date.setDate(date.getDate() + 1); // 日付を1日後に設定
+    const dateString = date.toISOString().split("T")[0];
     return trainingDates.includes(dateString) ? "bg-green-200" : "";
   };
 
