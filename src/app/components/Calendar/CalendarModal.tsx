27d26d5ee1a730aff02214e754904d0347b6edaa
kounoti <ac09606gc8xkh@gmail.com>
2024-05-17 19:35:53 +0900

    縦画面の時の表示修正

--- src/app/components/Calendar/CalendarModal.tsx
+++ src/app/components/Calendar/CalendarModal.tsx
@@ -50,58 +50,62 @@ const CalendarModal: React.FC<CalendarModalProps> = ({
   };
 
   return (
-    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white rounded-lg p-8 max-h-screen overflow-y-auto">
-        <h2 className="text-xl font-bold mb-4">トレーニング情報 - {date}</h2>
-        <table className="w-full bg-white border border-collapse border-gray-300 max-h-[80vh] shadow-md rounded-md">
-          <thead className="bg-teal-400 text-white">
-            <tr>
-              <th className="p-2 text-center border">トレーニングメニュー</th>
-              <th className="p-2 text-center border">重量</th>
-              <th className="p-2 text-center border">回数</th>
-              <th className="text-center border" style={{ width: "1%" }}>
-                {/* 削除ボタンの列の幅を調整 */}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {trainings.map((training) => (
-              <tr key={training.id}>
-                <td className="p-2 text-center border">
-                  {training.trainingMenu}
-                </td>
-                <td className="p-2 text-center border">{training.weight}kg</td>
-                <td className="p-2 text-center border">{training.count}回</td>
-                <td className="text-center border">
-                  {/* 投稿ボタン */}
-                  <button
-                    className="text-white bg-blue-400 hover:bg-blue-500 rounded-md p-1 flex items-center mb-1"
-                    onClick={(e) => addTimeLine(e, training)} // 投稿ボタンがクリックされたときにトレーニング情報を渡す
-                    style={{ width: "fit-content" }}
-                  >
-                    <SlSpeech className="mr-1 hidden sm:table-cell" />
-                    <span style={{ whiteSpace: "nowrap" }}>投稿</span>
-                  </button>
-                  {/* 削除ボタン */}
-                  <button
-                    className="text-white bg-red-300 hover:bg-red-400 rounded-md p-1 flex items-center"
-                    onClick={() => DeleteMemory(training.id)}
-                    style={{ width: "fit-content" }}
-                  >
-                    <RiDeleteBin6Fill className="mr-1 hidden sm:table-cell" />
-                    <span style={{ whiteSpace: "nowrap" }}>削除</span>
-                  </button>
-                </td>
+    <div className="my-10">
+      <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
+        <div className="bg-white rounded-lg p-8 max-h-screen overflow-y-auto">
+          <h2 className="text-xl font-bold mb-4">トレーニング情報 - {date}</h2>
+          <table className="w-full bg-white border border-collapse border-gray-300 max-h-[80vh] shadow-md rounded-md">
+            <thead className="bg-teal-400 text-white">
+              <tr>
+                <th className="p-2 text-center border">トレーニングメニュー</th>
+                <th className="p-2 text-center border">重量</th>
+                <th className="p-2 text-center border">回数</th>
+                <th className="text-center border" style={{ width: "1%" }}>
+                  {/* 削除ボタンの列の幅を調整 */}
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <button
-          className="mt-4 bg-blue-500 text-white py-2 px-4 rounded"
-          onClick={onClose}
-        >
-          閉じる
-        </button>
+            </thead>
+            <tbody>
+              {trainings.map((training) => (
+                <tr key={training.id}>
+                  <td className="p-2 text-center border">
+                    {training.trainingMenu}
+                  </td>
+                  <td className="p-2 text-center border">
+                    {training.weight}kg
+                  </td>
+                  <td className="p-2 text-center border">{training.count}回</td>
+                  <td className="text-center border">
+                    {/* 投稿ボタン */}
+                    <button
+                      className="text-white bg-blue-400 hover:bg-blue-500 rounded-md p-1 flex items-center mb-1"
+                      onClick={(e) => addTimeLine(e, training)} // 投稿ボタンがクリックされたときにトレーニング情報を渡す
+                      style={{ width: "fit-content" }}
+                    >
+                      <SlSpeech className="mr-1 hidden sm:table-cell" />
+                      <span style={{ whiteSpace: "nowrap" }}>投稿</span>
+                    </button>
+                    {/* 削除ボタン */}
+                    <button
+                      className="text-white bg-red-300 hover:bg-red-400 rounded-md p-1 flex items-center"
+                      onClick={() => DeleteMemory(training.id)}
+                      style={{ width: "fit-content" }}
+                    >
+                      <RiDeleteBin6Fill className="mr-1 hidden sm:table-cell" />
+                      <span style={{ whiteSpace: "nowrap" }}>削除</span>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <button
+            className="mt-4 bg-blue-500 text-white py-2 px-4 rounded"
+            onClick={onClose}
+          >
+            閉じる
+          </button>
+        </div>
       </div>
     </div>
   );
