11d323c721f2e0e148c8e4c0370b64a0817352b7
kounoti <ac09606gc8xkh@gmail.com>
2024-06-03 21:43:22 +0900

    不要なコメントアウトを削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,26 +11,6 @@ import Head from "next/head";
 // import TopPage from "./components/TopPage/TopPage";
 
 export default function Home() {
-  // const [currentUser, setCurrentUser] = useState<string | null>(null);
-
-  // // 現在ログインしているユーザーを取得する処理
-  // const getCurrentUser = async () => {
-  //   // ログインのセッションを取得する処理
-  //   const { data } = await supabase_google.auth.getSession();
-  //   // セッションがあるときだけ現在ログインしているユーザーを取得する
-  //   if (data.session !== null) {
-  //     // supabaseに用意されている現在ログインしているユーザーを取得する関数
-  //     const {
-  //       data: { user },
-  //     } = await supabase_google.auth.getUser();
-  //     // currentUserにユーザーのメールアドレスを格納
-  //     setCurrentUser(user?.email ?? null);
-  //   }
-  // };
-  // useEffect(() => {
-  //   getCurrentUser();
-  // }, []);
-
   return (
     <>
       <Head>
@@ -38,18 +18,7 @@ export default function Home() {
         <link rel="icon" href="/bear.png" />
       </Head>
 
-      {/* {currentUser === null ? <Authentication /> : <TopPage />} */}
-
       <Authentication />
-      {/* <div className="flex justify-center items-center m-10">
-        <Link
-          href="/components/Training"
-          className="bg-blue-300 hover:bg-blue-500 text-white font-bold py-10 px-10 rounded border-2 border-blue-400 sm:text-5xl text-2xl "
-        >
-          トレーニング開始
-                  </Link>
-
-      </div> */}
     </>
   );
 }
